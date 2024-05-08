dffe0a865801386cca4b81e9054ae3091dcefa9e
ankush25 <ankushthummar25@gmail.com>
2024-05-08 10:47:14 +0530

    08-05-2024

---
+++ Import_Export/basic.js
@@ -0,0 +1,47 @@
+const sum = (a,b)=>{
+    return a + b;
+}
+
+// console.log(sum(2,6));              // 8
+
+exports.sum = sum;
+
+
+
+
+
+
+exports.man =(a,b)=>{
+    return a + b;
+}
+
+// console.log(man(10,8));             // 18
+
+
+
+
+
+// function dif (a,b){
+//     return a-b;
+// }
+// console.log(dif(3,8));              // -5
+
+exports.dife =(a,b)=>{
+    return a-b;
+}
+
+
+
+// function mul(s,v){
+//     return s*v;
+// }
+// console.log(mul(7,7));             // 49
+
+exports.multi=(s,v)=>{
+        return s*v;
+    }
+
+
+
+
+// export {mul,dif};
---
+++ Import_Export/import.js
@@ -0,0 +1,24 @@
+const demo = require("./basic.js");
+
+console.log(demo.sum(23,89));                 // 112
+
+
+
+
+
+
+const use = require("./basic.js");
+console.log(use.man(32,70));                  // 102
+
+
+
+
+
+const ab = require("./basic.js");
+console.log(ab.dife(70,90));                   // -20
+
+
+
+
+const uk = require("./basic.js");
+console.log(uk.multi(90,7));                  // 630
