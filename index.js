8a592fa9ad1ba972988f47324710fed1955b2556
ankush25 <ankushthummar25@gmail.com>
2024-05-11 10:51:47 +0530

    11-05-2024

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const http = require('http');
 
+
+const fs = require('fs');
+
+
+//   //  => HTML Read
+// const readData = fs.readFileSync('index.html','utf-8')
+
+
+//   //  => JSON Read
+const readData = fs.readFileSync('product.json','utf-8')
+
+
+
+
+
+// const data = {
+//     age :20
+// }
+
+
 const server = http.createServer((req,res)=>{
     console.log('server Created');
-    res.end('<h1> Hello </h1>');
+
+    //  // HTML
+    // res.setHeader('Content-Type','text/html');
+    // res.end('<h1> Hello </h1>');
+
+
+    //  // JSON
+    // res.setHeader('Content-Type','application/json');
+    // res.end(JSON.stringify(data));
+
+
+
+    res.end(readData);
+
 })
 
 server.listen(9090);
